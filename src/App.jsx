59b4c589492290b66970fd626802e5cc7487ae0a
Haseeb Khalid <david.brandboosters@gmail.com>
2024-05-06 23:12:52 +0500

    pages

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import { Routes, Route } from "react-router-dom";
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import 'swiper/css';
-import './App.css
+import './App.css'
 
 
 function App() {
